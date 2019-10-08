8b9ee68b349882c12749b1a0375029a0cbc97fce
fsuffieldcode <fsuffieldcode@gmail.com>
2019-10-08 22:15:22 +0100

    add new todo functionality

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,46 +21,19 @@ app.get("/", (req, res) => {
 
     var day = today.toLocaleDateString("en-GB", options);
 
-    res.render('list', {
-        kindOfDay: day
-    });
+    res.render("list", {kindOfDay: day, newListItems: items});
     
-
 });
 
 app.post('/', (req, res) => {
-    const newTodo = req.body.newItem
-    console.log(newTodo);
+    var item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
   });
 
 
 app.listen(3000, () =>{
     console.log("Server started on port 3000");
 });
-
-
-
-
-// switch(day) {
-//     case 0:
-//         day = "Sunday";
-//         break;
-//     case 1:
-//         day = "Monday";
-//         break;
-//     case 2:
-//         day = "Tuesday";
-//         break;
-//     case 3:
-//         day = "Wednesday";
-//         break;
-//     case 4:
-//         day = "Thursday";
-//         break;
-//     case 5:
-//         day = "Friday";
-//         break;
-//     case 6:
-//         day = "Saturday";
-//         break;
-// }
