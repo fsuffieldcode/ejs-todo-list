216400267e3748d353ab2b534b51338f5cc97dfe
fsuffieldcode <fsuffieldcode@gmail.com>
2019-10-08 13:47:24 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    var today = new Date();
+    day = today.getDay();
+    switch(day) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+    }
+
+    res.render('list', {kindOfDay: day});
+    
+
+});
+
+
+app.listen(3000, () =>{
+    console.log("Server started on port 3000");
+});
+
