25469b7b725c235fe2deebe83dde2ae459154925
fsuffieldcode <fsuffieldcode@gmail.com>
2019-10-08 20:05:47 +0100

    add new todo function

--- app.js
+++ app.js
@@ -5,40 +5,60 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
     var today = new Date();
     day = today.getDay();
-    switch(day) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-    }
 
-    res.render('list', {kindOfDay: day});
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-GB", options);
+
+    res.render('list', {
+        kindOfDay: day
+    });
     
 
 });
+
+app.post('/', (req, res) => {
+    const newTodo = req.body.newItem
+    console.log(newTodo);
+  });
 
 
 app.listen(3000, () =>{
     console.log("Server started on port 3000");
 });
 
+
+
+
+// switch(day) {
+//     case 0:
+//         day = "Sunday";
+//         break;
+//     case 1:
+//         day = "Monday";
+//         break;
+//     case 2:
+//         day = "Tuesday";
+//         break;
+//     case 3:
+//         day = "Wednesday";
+//         break;
+//     case 4:
+//         day = "Thursday";
+//         break;
+//     case 5:
+//         day = "Friday";
+//         break;
+//     case 6:
+//         day = "Saturday";
+//         break;
+// }
