b47fe818a4f703cb5e78fd0c086d19c4a7462787
fsuffieldcode <fsuffieldcode@gmail.com>
2019-10-09 15:24:53 +0100

    add css styling

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", (req, res) => {
     let today = new Date();
     let day = today.getDay();
